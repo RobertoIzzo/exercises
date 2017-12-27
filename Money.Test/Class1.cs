c92b0c049ca2bbf71bac13490020f6b9bb0c1ca9
robrob1 <roberto.izzo@gmail.com>
2017-12-28 00:03:10 +0100

    tdd

---
+++ Money.Test/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Money.Test
+{
+
+    public class Class1
+    {
+        [Fact]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Assert.Equal(five.Times(2), new Dollar(10));
+            Assert.Equal(five.Times(3), new Dollar(15));
+        }
+
+
+        [Fact]
+        public void TestFrancMultiplication()
+        {
+            Franc five = new Franc(5);
+            Assert.Equal(five.Times(2), new Franc(10));
+            Assert.Equal(five.Times(3), new Franc(15));
+        }
+        [Fact]
+        public void TestEquality()
+        {
+            Assert.True(new Dollar(5).Equals(new Dollar(5)));
+            Assert.False(new Dollar(5).Equals(new Dollar(6)));
+            Assert.True(new Franc(5).Equals(new Franc(5)));
+            Assert.False(new Franc(5).Equals(new Franc(6)));
+            Assert.False(new Franc(5).Equals(new Dollar(5)));
+
+        }
+    }
+    
+}
---
+++ Money/Money.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class Money
+    {
+        protected int _amount;
+
+        public override bool Equals(object obj)
+        {
+            Money money = (Money)obj;
+            return _amount == money._amount
+                && GetType() == money.GetType();
+        }
+
+
+    }
+}
