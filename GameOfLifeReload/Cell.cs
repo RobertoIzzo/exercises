30fa1e474994ebc130db32acc0bbc9c90fb69468
robrob1 <roberto.izzo@gmail.com>
2018-02-16 00:59:53 +0100

    add GameOfLifeReload

---
+++ GameOfLifeReload/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeReload
+{
+    public class Cell
+    {
+        public Cell(int x, int y)
+        {
+            PositionX = x;
+            PositionY = y;
+        }
+        public void SetLive()
+        {
+            IsLive = true;
+        }
+
+        public bool IsLive { get; protected set; }
+        public int PositionX { get; protected set; }
+        public int PositionY { get; protected set; }
+        public int NeighborsLive { get; protected set; }
+
+        public void Search(Cell[][] word)
+        {
+            NeighborsLive = word[PositionX][PositionY - 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+            NeighborsLive = word[PositionX][PositionY + 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+        }
+    }
+}
---
+++ GameOfLifeReloadTest/GameTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLifeReload;
+using Xunit;
+
+namespace GameOfLifeReloadTest
+{
+    [Category("Unit")]
+    public class GameTest
+    {
+        [Fact]
+        public void GivenLiveCell_WhenSetLive_ReturnLive()
+        {
+            //ARRANGE
+            Cell cell = new Cell(1,1);
+
+            //ACT
+            cell.SetLive();
+
+            //ASSERT   
+            Assert.True(cell.IsLive);
+        }
+
+        [Fact]
+        public void Given2LiveNeighbors_WhenCellSearchNeighborsLive_Return2LiveNeighbors()
+        {
+            //ARRANGE
+            Cell sut = new Cell(1,1);
+            Cell[][] word = Mother.Given2LiveNeighbors(sut);
+
+            //ACT
+            sut.Search(word);
+
+            //ASSERT
+            Assert.True(sut.NeighborsLive == 2);
+        }
+
+
+        [Fact]
+        public void Given3LiveNeighbors_WhenCellSearchNeighborsLive_Return3LiveNeighbors()
+        {
+            //ARRANGE
+            Cell sut = new Cell(1, 1);
+            Cell[][] word = Mother.Given3LiveNeighbors(sut);
+
+            //ACT
+            sut.Search(word);
+
+            //ASSERT
+            Assert.True(sut.NeighborsLive == 3);
+        }
+
+    }
+
+    public class Mother
+    {
+
+       public static Cell[][] Given2LiveNeighbors(Cell cell)
+        {
+            Cell[][] word = new Cell[3][];
+            word[0] = new Cell[3];
+            word[1] = new Cell[3];
+            word[2] = new Cell[3];
+            word[1][1] = cell;
+            Cell left = new Cell(1,0);
+            left.SetLive();
+            word[1][0] = left;
+            Cell right = new Cell(1,2);
+            right.SetLive();
+            word[1][2] = right;
+           return word;
+        }
+
+        public static Cell[][] Given3LiveNeighbors(Cell cell)
+        {
+            Cell[][] word = new Cell[3][];
+            word[0] = new Cell[3];
+            word[1] = new Cell[3];
+            word[2] = new Cell[3];
+            word[1][1] = cell;
+
+            Cell bottomsx = new Cell(0, 0);
+            bottomsx.SetLive();
+            word[0][0] = bottomsx;
+
+            Cell left = new Cell(1,0);
+            left.SetLive();
+            word[1][0] = left;
+
+            Cell topsx = new Cell(2, 0);
+            topsx.SetLive();
+            word[2][0] = topsx;
+            return word;
+        }
+    }
+}
