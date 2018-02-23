273d7ff75e263b9dfdd27d081ecbfcbbe01ff7bf
INT\roberto <mylerch@hotmail.it>
2018-02-23 09:55:27 +0100

    first commit

---
+++ WebSite1/WebApplicationMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            TestClass test= new TestClass();
+            var result = await test.DoLongTask();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+  
+}
