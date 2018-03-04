040aa10b43220a5985e94fb25756c0c66be6a2a9
robrob1 <roberto.izzo@gmail.com>
2018-03-04 16:20:56 +0100

    end tdd test

--- GameOfLifeReload/Cell.cs
+++ GameOfLifeReload/Cell.cs
@@ -6,27 +6,77 @@ using System.Threading.Tasks;
 
 namespace GameOfLifeReload
 {
+
+    public interface ICell
+    {
+        void Search(Cell[][] word);
+        void Convert(Cell[][] word);
+    }
+
     public class Cell
     {
-        public Cell(int x, int y)
-        {
-            PositionX = x;
-            PositionY = y;
-        }
-        public void SetLive()
-        {
-            IsLive = true;
-        }
 
         public bool IsLive { get; protected set; }
         public int PositionX { get; protected set; }
         public int PositionY { get; protected set; }
         public int NeighborsLive { get; protected set; }
 
+        public Cell(int x, int y)
+        {
+            PositionX = x;
+            PositionY = y;
+        }
+
+        public void SetLive()
+        {
+            IsLive = true;
+        }
+
         public void Search(Cell[][] word)
         {
-            NeighborsLive = word[PositionX][PositionY - 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
-            NeighborsLive = word[PositionX][PositionY + 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+            //top
+            if (word[PositionX - 1][PositionY] != null)
+                NeighborsLive = word[PositionX - 1][PositionY].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //bottom
+            if (word[PositionX + 1][PositionY] != null)
+                NeighborsLive = word[PositionX + 1][PositionY].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //sin
+            if (word[PositionX][PositionY - 1] != null)
+                NeighborsLive = word[PositionX][PositionY - 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //dest
+            if (word[PositionX][PositionY + 1] != null)
+                NeighborsLive = word[PositionX][PositionY + 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //top sx 
+            if (word[PositionX - 1][PositionY - 1] != null)
+                NeighborsLive = word[PositionX - 1][PositionY - 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //bottom sx 
+            if (word[PositionX + 1][PositionY - 1] != null)
+                NeighborsLive = word[PositionX + 1][PositionY - 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //top dx 
+            if (word[PositionX - 1][PositionY + 1] != null)
+                NeighborsLive = word[PositionX - 1][PositionY + 1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+            //bottom dx 
+            if (word[PositionX + 1][PositionY + 1] != null)
+                NeighborsLive = word[PositionX + 1][PositionY - +1].IsLive ? NeighborsLive + 1 : NeighborsLive;
+
+        }
+
+        public void Convert(Cell[][] word)
+        {
+            Search(word);
+            if (!IsLive && NeighborsLive == 3)
+                IsLive = true;
+            if (IsLive && NeighborsLive < 2)
+                IsLive = false;
+            if (IsLive && NeighborsLive > 3)
+                IsLive = false;
         }
     }
 }
--- GameOfLifeReloadTest/GameTest.cs
+++ GameOfLifeReloadTest/GameTest.cs
@@ -39,7 +39,6 @@ namespace GameOfLifeReloadTest
             Assert.True(sut.NeighborsLive == 2);
         }
 
-
         [Fact]
         public void Given3LiveNeighbors_WhenCellSearchNeighborsLive_Return3LiveNeighbors()
         {
@@ -54,18 +53,78 @@ namespace GameOfLifeReloadTest
             Assert.True(sut.NeighborsLive == 3);
         }
 
+        [Fact]
+        public void GivenDeadCellWith3LiveNeighbors_WhenConvert_ReturnIsAlveTrue()
+        {
+            //ARRANGE
+            Cell sut = new Cell(1, 1);
+            Cell[][] word = Mother.Given3LiveNeighbors(sut);
+            
+            //ACT
+            sut.Convert(word);
+
+            //ASSERT
+            Assert.True(sut.IsLive);
+        }
+
+        [Fact]
+        public void GivenLiveCellWith1LiveNeighbors_WhenConvert_ReturnIsAlveFalse()
+        {
+            //ARRANGE
+            Cell sut = new Cell(1, 1);
+            Cell[][] word = Mother.Given1LiveNeighbors(sut);
+            sut.SetLive();
+
+            //ACT
+            sut.Convert(word);
+
+            //ASSERT
+            Assert.False(sut.IsLive);
+        }
+
+        [Fact]
+        public void GivenLiveCellWith4LiveNeighbors_WhenConvert_ReturnIsAlveFalse()
+        {
+            //ARRANGE
+            Cell sut = new Cell(1, 1);
+            Cell[][] word = Mother.Given4LiveNeighbors(sut);
+            sut.SetLive();
+
+            //ACT
+            sut.Convert(word);
+
+            //ASSERT
+            Assert.False(sut.IsLive);
+        }
+
     }
 
     public class Mother
     {
-
-       public static Cell[][] Given2LiveNeighbors(Cell cell)
+        public static Cell[][] Given1LiveNeighbors(Cell cell)
         {
             Cell[][] word = new Cell[3][];
             word[0] = new Cell[3];
             word[1] = new Cell[3];
             word[2] = new Cell[3];
             word[1][1] = cell;
+
+            Cell left = new Cell(1, 0);
+            left.SetLive();
+            word[1][0] = left;
+            Cell right = new Cell(1, 2);
+            word[1][2] = right;
+            return word;
+        }
+
+        public static Cell[][] Given2LiveNeighbors(Cell cell)
+        {
+            Cell[][] word = new Cell[3][];
+            word[0] = new Cell[3];
+            word[1] = new Cell[3];
+            word[2] = new Cell[3];
+            word[1][1] = cell;
+
             Cell left = new Cell(1,0);
             left.SetLive();
             word[1][0] = left;
@@ -83,17 +142,43 @@ namespace GameOfLifeReloadTest
             word[2] = new Cell[3];
             word[1][1] = cell;
 
-            Cell bottomsx = new Cell(0, 0);
+            Cell bottomsx = new Cell(2, 0);
             bottomsx.SetLive();
-            word[0][0] = bottomsx;
+            word[2][0] = bottomsx;
 
             Cell left = new Cell(1,0);
             left.SetLive();
             word[1][0] = left;
 
-            Cell topsx = new Cell(2, 0);
+            Cell topsx = new Cell(0, 0);
             topsx.SetLive();
-            word[2][0] = topsx;
+            word[0][0] = topsx;
+            return word;
+        }
+
+        public static Cell[][] Given4LiveNeighbors(Cell cell)
+        {
+            Cell[][] word = new Cell[3][];
+            word[0] = new Cell[3];
+            word[1] = new Cell[3];
+            word[2] = new Cell[3];
+            word[1][1] = cell;
+
+            Cell bottomsx = new Cell(2, 0);
+            bottomsx.SetLive();
+            word[2][0] = bottomsx;
+
+            Cell left = new Cell(1, 0);
+            left.SetLive();
+            word[1][0] = left;
+
+            Cell topsx = new Cell(0, 0);
+            topsx.SetLive();
+            word[0][0] = topsx;
+
+            Cell top = new Cell(0,1);
+            top.SetLive();
+            word[0][1] = top;
             return word;
         }
     }
